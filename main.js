c726f2ada6127934d2827acf32c6fbf79904083f
Luis Henrique da Silva Campos <luis.henrique_campos@outlook.com.br>
2021-09-21 08:46:50 -0300

    entrada com os comandos aprendidos na aula do curso da DIO

---
+++ main.js
@@ -0,0 +1,63 @@
+// ## Introdução ao JavaScript
+// var nome = "Luis Henrique";
+// var idade = 29;
+// var idade2 = 10;
+// var n1 = 9;
+// var n2 = 2;
+// var frase = "japão é o melhor time do mundo !!!";
+// alert( nome + " tem " + idade + " anos ");
+// alert(idade +idade2);
+// console.log(nome);
+// console.log(idade+idade2);
+// console.log(frase);
+// console.log(n1*n2);
+// console.log(frase.replace("japão","Brasil"));
+// alert(frase.replace("japão","Brasil"));
+// alert(frase.toLowerCase());
+// alert(frase.toUpperCase());
+
+// ## Array e Dicionário
+
+// var lista = ["maçã","pera","laranja"];
+// lista.push("uva");
+// lista.pop();
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" - "))
+// var fruta = {nome:"maçã",cor:"vermelha"}
+// console.log(fruta.nome);
+// console.log(fruta.cor);
+// var frutas = [{nome:"maçã",cor:"vermelha"}, {nome:"uva",cor:
+// "roxa"}]
+// console.log(frutas[0].nome);
+// console.log(frutas[1].cor);
+
+
+// ## Laço de repetição e date
+
+// var idade = 18;
+// var idade = prompt ("Qual sua idade ");
+// if (idade >= 18){
+// alert("Maior de idade!");
+// }else{
+//   alert("Menor de idade!");
+// }
+
+// var count = 0;
+// while(count <= 5){
+//   console.log(count);
+//   alert(count);
+//   count ++;
+// };
+
+// var count;
+// for(count = 0; count <= 5; count ++){
+//   alert(count);
+// };
+
+// var d = new Date();
+// alert(d.getHours());
+// alert(d.getMinutes());
+// alert(d.getDay()+1);
+// alert(d.getMonth()+1);
